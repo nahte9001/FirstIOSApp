258fddb6eea3ba89458b94646901561dedc984f6
Ethan Torres <nahte9001@gmail.com>
2018-10-09 08:44:21 -0600

    Changed Storyboard and added a file.

---
+++ FirstIOSApp/Model/Colortool.swift
@@ -0,0 +1,15 @@
+//
+//  Colortool.swift
+//  FirstIOSApp
+//
+//  Created by Torres, Ethan on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
